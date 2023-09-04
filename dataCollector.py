84b4960ec264803767f9d94e396b7a4ac5161f5b
leonkuessner <leonkuessner1@gmail.com>
2023-09-04 23:42:46 +0200

    Added further indeces and ability to add other ones in the future

--- dataCollector.py
+++ dataCollector.py
@@ -4,10 +4,14 @@ from dateutil.relativedelta import relativedelta
 import urllib
 import json
 import requests
+from functools import reduce
 
 url_walcl = "https://fred.stlouisfed.org/graph/fredgraph.csv?id=WALCL&cosd={}"
 url_rem = "https://fred.stlouisfed.org/graph/fredgraph.csv?id=RESPPLLOPNWW&cosd={}"
 url_spx = "https://fred.stlouisfed.org/graph/fredgraph.csv?id=SP500&cosd={}"
+url_wsc = "https://fred.stlouisfed.org/graph/fredgraph.csv?id=WILLSMLCAP&cosd={}"
+url_wmc = "https://fred.stlouisfed.org/graph/fredgraph.csv?id=WILLMIDCAP&cosd={}"
+url_wlc = "https://fred.stlouisfed.org/graph/fredgraph.csv?id=WILLLRGCAP&cosd={}"
 url_tga = "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/dts/dts_table_1?filter=record_date:gte:{},account_type:eq:Treasury%20General%20Account%20(TGA)%20Opening%20Balance&fields=record_date,open_today_bal&page[number]=1&page[size]=900"
 url_rrp = "https://markets.newyorkfed.org/api/rp/reverserepo/propositions/search.json?startDate={}"
 class CustomError(Exception):
@@ -19,7 +23,11 @@ class NetLiquidityCalculation:
         self.start_date = start_date
         self.walcl = self.get_csvs(url_walcl, "WALCL") * 1_000_000
         self.rem = self.get_csvs(url_rem,"REM") * 1_000_000
+
         self.spx = self.get_csvs(url_spx,"SPX")
+        self.wsc = self.get_csvs(url_wsc,"WSC")
+        self.wmc = self.get_csvs(url_wmc,"WMC")
+        self.wlc = self.get_csvs(url_wlc,"WLC")
 
         self.tga = self.get_tga()
         self.rrp = self.get_rrp()
@@ -71,16 +79,30 @@ class NetLiquidityCalculation:
         return df.sort_index()
 
     def calculate_net_liquidity(self):
-        df = self.rrp.join(self.walcl).join(self.tga).join(self.rem).join(self.spx)
+        dfs = [self.rrp, self.walcl, self.tga, self.rem, self.spx, self.wsc, self.wmc, self.wlc]
+
+        def left_join_df(left, right):
+            return left.join(right, how='left')
+        df = reduce(left_join_df, dfs)
+
         df = df.ffill()
-        df["NL"] = df["WALCL"] - df[["TGA", "REM", "RRP"]].sum(axis=1)
-        df["SPX_FV"] = df["NL"] / 1_000_000_000 / 1.1 - 1625
-        df["SPX_HI"] = df["SPX_FV"] + 300
-        df["SPX_LO"] = df["SPX_FV"] - 200
-        df = df.dropna(how="any")
         df = df.loc[df["RRP"] != 0]
+        df["NL"] = df["WALCL"] - df[["TGA", "REM", "RRP"]].sum(axis=1)
+        return df
+
+    def updater(self, tracker, skew=1.1, threshold=0.07):
+        df = self.calculate_net_liquidity()
+        aligner_dict = {
+            "spx": [1_000_000_000, 1625],
+            "wsc": [30_000_000, 30_000],
+            "wmc": [30_000_000, 5_000],
+            "wlc": [30_000_000, 50_000]
+        }
+        df["FV"] = df["NL"] / aligner_dict[tracker][0] / skew - aligner_dict[tracker][1]
+        df["HI"] = df["FV"] * (1 + threshold)
+        df["LO"] = df["FV"] * (1 - threshold)
         return df
 
 if __name__ == '__main__':
-    NetLiquidityCalculation(date(2023,7,1)).calculate_net_liquidity()
+    df = NetLiquidityCalculation(date(2022,1,1)).updater(tracker="wmc")
 
--- frontend.py
+++ frontend.py
@@ -6,6 +6,7 @@ from plotly.subplots import make_subplots
 from dash import Dash, html, dcc, callback, Output, Input, State, ctx
 import dash_bootstrap_components as dbc
 from dash_bootstrap_templates import ThemeSwitchAIO
+from dash.dcc import Download
 
 import sys
 import os
@@ -14,20 +15,18 @@ import dataCollector as dc
 import plotly_chart_formatting as pcf
 
 #-------------------------- DATA AND PLOTTING
-nlc = dc.NetLiquidityCalculation((dt.now() - relativedelta(months=15)).date())
-df = nlc.calculate_net_liquidity()
+nlc = dc.NetLiquidityCalculation(date(2022,5,1))
+df = nlc.updater(tracker="spx")
 
 namings = {
     "SPX": "S&P 500",
-    "SPX_FV": "Fair Value",
-    "SPX_HI": "Upper Bound",
-    "SPX_LO": "Lower Bound",
+    "WSC": "Wilshire Small Cap Total Index",
+    "WMC": "Wilshire Mid Cap Total Index",
+    "WLC": "Wilshire Large Cap Total Index",
+    "FV": "Fair Value",
+    "HI": "Upper Bound",
+    "LO": "Lower Bound",
 }
-
-main_fig = go.Figure()
-main_fig.add_traces(data=[
-    go.Scatter(x = df.index, y = df[trace], name=namings[trace]) for trace in ["SPX", "SPX_LO", "SPX_FV", "SPX_HI"]
-]).update_layout(pcf.layout_dict)
 
 
 
@@ -45,10 +44,6 @@ app.layout = html.Div([
             dbc.Col([
                 html.H5("Leon Kuessner")
             ], width=5, align="center", style={"text-align": "right"}),
-            # dbc.Col([
-            #     html.P("Inspiration:"),
-            #     dcc.Link("Dharmatrade", href="https://twitter.com/dharmatrade")
-            # ], width=2, align="center", style={"text-align": "center"}),
             dbc.Col([
                 ThemeSwitchAIO(
                     aio_id="theme", themes=[dbc.themes.CYBORG, dbc.themes.COSMO]
@@ -58,14 +53,33 @@ app.layout = html.Div([
         ], justify="center"),
         dbc.Row([
             dbc.Col([
-                dbc.Button("Download Data", id="download_data_button")
-            ], width=3),
+                dbc.RadioItems(
+                    id="main_chart_radios",
+                    className="btn-group",
+                    inputClassName="btn-check",
+                    labelClassName="btn btn-outline-primary",
+                    labelCheckedClassName="active",
+                    options=[
+                        {"label": "S&P 500", "value": "spx"},
+                        {"label": "Small Caps", "value": "wsc"},
+                        {"label": "Mid Caps", "value": "wmc"},
+                        {"label": "Large Caps", "value": "wlc"},
+                    ],
+                    value="spx"
+                )
+            ], width={"size": 8, "offset": 4}, className="radio-group")
+        ], justify="center", style={"marginTop": 20, "marginBottom": 10}),
+        dbc.Row([
+            dbc.Col([
+                dbc.Button("Download Data", id="download_data_button"),
+                dcc.Download(id="download_df")
+            ], width={"size":4, "offset": 2}),
         ], justify="center"),
         dbc.Row([
             dbc.Col([
                 dcc.Graph(id="main_figure")
             ], width=10),
-        ], justify="center"), # MAIN CHART ROW
+        ], justify="center", style={"marginTop": 20}), # MAIN CHART ROW
         dbc.Row([
             dbc.Row([
                 dbc.Col([
@@ -83,54 +97,30 @@ app.layout = html.Div([
                         ],
                         value="walcl"
                     )
-                ], width=5, className="radio-group")
-            ], justify="center"),
+                ], width={"size": 4, "offset": 2}, className="radio-group")
+            ], justify="center", style={"marginTop": 20, "marginBottom": 10}),
             dbc.Col([
                 dcc.Graph(id="side_figure")
             ], width=10),
         ], justify="center"), # SUB CHART ROW
     ], justify="center"),
-    # dcc.Store(id="side_figure_store")
+    dcc.Store(data=df.to_dict("records"), id="df_store")
 ])
 
-# @app.callback(
-#     Output("side_figure", "figure"),
-#     Input("side_chart_radios", "value"),
-#     State(ThemeSwitchAIO.ids.switch("theme"), "value"),
-# )
-# def create_sub_chart(radio_val, toggle):
-#     template = "cyborg" if toggle else "cosmo"
-#     data_dict = {
-#         "walcl": nlc.walcl,
-#         "tga": nlc.tga,
-#         "rrp": nlc.rrp,
-#         "rem": nlc.rem,
-#     }
-#     df = data_dict[radio_val].dropna(how="any")
-#     return side_fig.add_trace(go.Scatter(x=df.index, y=df[df.columns[0]])).update_layout(template=template)
-#
-# @app.callback(
-#     Output("main_figure", "figure"),
-#     Output("side_figure", "figure"),
-#     Input(ThemeSwitchAIO.ids.switch("theme"), "value"),
-#     State("side_figure", "figure")
-# )
-# def update_graph_theme(toggle, side_fig):
-#     template = "cyborg" if toggle else "cosmo"
-#     # return px.line(df, x="date", y="GOOG", template=template)
-#     top_fig = main_fig.update_layout(template=template)
-#     sub_fig = side_fig.update_layout(template=template)
-#     return top_fig, sub_fig
 @app.callback(
     Output("main_figure", "figure"),
     Output("side_figure", "figure"),
+    Output("df_store", "data"),
     Input(ThemeSwitchAIO.ids.switch("theme"), "value"),
     Input("side_chart_radios", "value"),
-    State("side_figure", "figure")
+    Input("main_chart_radios", "value"),
+    State("side_figure", "figure"),
+    State("main_figure", "figure"),
+    State("df_store", "data"), ## To pass through the same values if no change
 )
-def update_graph_theme(toggle, side_chart_radios, existing_side_fig):
+def update_graph_theme(toggle, side_chart_radios, main_chart_radios, existing_side_fig, existing_main_fig, df_store):
     template = "cyborg" if toggle else "cosmo"
-    if ctx.triggered_id == "side_chart_radios" or not ctx.triggered_id:
+    def side_fig_creator():
         data_dict = {
             "walcl": nlc.walcl,
             "tga": nlc.tga,
@@ -139,16 +129,33 @@ def update_graph_theme(toggle, side_chart_radios, existing_side_fig):
         }
         df = data_dict[side_chart_radios].dropna(how="any")
         side_fig = go.Figure()
-        sub_fig = side_fig.add_trace(go.Scatter(x=df.index, y=df[df.columns[0]]))
-        sub_fig = sub_fig.update_layout(pcf.layout_dict,template=template, title=dict(text=side_chart_radios.upper(), x=0.5))
+        side_fig = side_fig.add_trace(go.Scatter(x=df.index, y=df[df.columns[0]]))
+        side_fig = side_fig.update_layout(pcf.layout_dict,template=template, title=dict(text=side_chart_radios.upper(), x=0.5))
+        return side_fig
+
+    def main_fig_creator():
+        new_df = nlc.updater(main_chart_radios)
+        df_store = new_df.to_dict("records")
+        top_fig = go.Figure()
+        top_fig.add_traces(data=[
+            go.Scatter(x=new_df.index, y=new_df[trace], name=namings[trace]) for trace in
+            [main_chart_radios.upper(), "LO", "FV", "HI"]
+        ]).update_layout(pcf.layout_dict, template=template)
+        return top_fig, df_store
+
+    if ctx.triggered_id == "side_chart_radios" or not ctx.triggered_id:
+        side_fig = side_fig_creator()
         if not ctx.triggered_id:
-            top_fig = main_fig.update_layout(template=template)
+            top_fig, df_store = main_fig_creator()
         else:
-            top_fig = main_fig
+            top_fig = existing_main_fig
+    elif ctx.triggered_id == "main_chart_radios":
+        top_fig, df_store = main_fig_creator()
+        side_fig = existing_side_fig
     else:
-        top_fig = main_fig.update_layout(template=template)
-        sub_fig = go.Figure(existing_side_fig).update_layout(template=template)
-    return top_fig, sub_fig
+        top_fig = existing_main_fig.update_layout(template=template)
+        side_fig = go.Figure(existing_side_fig).update_layout(template=template)
+    return top_fig, side_fig, df_store
 
 if __name__ == '__main__':
     app.run(debug=True)
